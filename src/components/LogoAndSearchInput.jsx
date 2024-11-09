f39089c35d1b0c8e1c6116504f8bd85d1b0ee9b4
whgmlwls0219 <whgmlwls0219@naver.com>
2024-11-10 00:16:19 +0900

    검색 tbx, 검색 btn 디자인 재수정

--- src/components/LogoAndSearchInput.jsx
+++ src/components/LogoAndSearchInput.jsx
@@ -14,98 +14,58 @@ const TitleLogo = styled.div`
   margin: 2.5rem 5rem 4rem 3rem;
 `;
 
-// // 검색 tbx, 검색 btn 감싸는 div
-// const SearchInputDiv = styled.div`
-//   position: absolute;
-//   display: inline-block;
-//   top: 50%;
-//   left: 50%;
-//   transform: translate(-50%, -50%);
-// `;
+// 검색 tbx, 검색 btn 감싸는 div
+const SearchDiv = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: 3px solid black;
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  justify-content: space-evenly;
+  width: 380px;
+  height: 45px;
+  border-radius: 15px;
+`;
 
-// // 검색 tbx
-// const SearchTbx = styled.input`
-//   height: 49px;
-//   border-radius: 10px;
-//   width: 350px;
-//   padding: 0px 10px 0px 10px;
-//   font-size: 20px;
-//   margin-bottom: 6px;
-//   border: 3px solid green;
-// `;
-
-// // 검색 btn
-// const SearchBox = styled.button`
-//   height: 51px;
-//   border-radius: 10px;
-//   margin-left: 7px;
-//   font-size: 18px;
-//   cursor: pointer;
-//   width: 60px;
-//   border: 1px solid black;
-// `;
+// 검색 tbx
+const SearchTbx = styled.input`
+  width: 300px;
+  height: 35px;
+  border-radius: 10px;
+  /* border: none;
+  font-size: 18px;
+  &:focus {
+    border: 3px solid transparent;
+    &:active {
+      border: 3px solid transparent;
+    }
+  } */
+`;
 
 const LogoAndSearchInput = () => {
+  const MakeBorderNone = () => {
+    document.getElementById("idIsSearchBox").style.border =
+      "2px solid transparent";
+  };
+
   return (
     <>
       <WholeDiv>
         <TitleLogo>
           <img src="/image/ticketLinkImg.png" />
         </TitleLogo>
-        {/* <SearchInputDiv>
+        <SearchDiv>
           <SearchTbx
             type="text"
             placeholder="검색어를 입력해주세요"
+            id="idIsSearchBox"
+            onClick={MakeBorderNone}
           ></SearchTbx>
-          <SearchBox>검색</SearchBox>
-          <div
-            style={{
-              border: "2px solid red",
-              display: "inline-block",
-            }}
-          >
-            
-            
-          </div>
-        </SearchInputDiv> */}
-        <div
-          style={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            border: "3px solid black",
-            display: "flex",
-            alignItems: "center",
-            margin: "0 auto",
-            justifyContent: "space-evenly",
-            width: "380px",
-            height: "45px",
-            borderRadius: "15px",
-          }}
-        >
-          <div>
-            <input
-              type="text"
-              style={{
-                width: "325px",
-                height: "35px",
-                borderRadius: "10px",
-                padding: "0px 5px 0px 5px",
-                border: "none",
-              }}
-            ></input>
-          </div>
-          <div
-            style={{
-              display: "inline-block",
-              cursor: "pointer",
-              marginRight: "5px",
-            }}
-          >
-            <FontAwesomeIcon icon={faMagnifyingGlass} size="2xl" />
-          </div>
-        </div>
+          <FontAwesomeIcon icon={faMagnifyingGlass} size="2xl" />
+        </SearchDiv>
       </WholeDiv>
     </>
   );
