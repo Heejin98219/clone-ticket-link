efd63bd64ae3e6d21d39911b595d0e73dfb38bef
whgmlwls0219 <whgmlwls0219@naver.com>
2024-11-19 00:48:51 +0900

    공연 데이터 불러오는 작업 시작

--- src/components/LogoAndSearchInput.jsx
+++ src/components/LogoAndSearchInput.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 
 // 전체를 감싸는 div
@@ -45,9 +45,32 @@ const SearchTbx = styled.input`
 
 const LogoAndSearchInput = () => {
   const [searchWord, setSearchWord] = useState("");
-  const [result, setResult] = useState([]);
 
-  const BASE_URL = "http://kopis.or.kr/openApi/restful";
+  const BASE_URL = "http://kopis.or.kr/openApi/restful/pblprfr";
+  const API_KEY = process.env.REACT_API_KEY;
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/pblprfr`, {
+        params: {
+          service: API_KEY,
+          stdate: "20230601", // 시작 날짜 (필요시 변경 가능)
+          eddate: "20230630", // 종료 날짜 (필요시 변경 가능)
+          cpage: 1, // 현재 페이지
+          rows: 10, // 한 번에 가져올 데이터 개수
+          prfstate: "02", // 공연 상태
+          signgucode: "11", // 지역 코드
+          signgucodesub: "1111", // 하위 지역 코드
+          kidstate: "Y", // 어린이 공연 여부
+          keyword: searchWord, // 검색어
+        },
+      });
+
+      setResult(response.data); // 결과를 상태에 저장
+    } catch (error) {
+      console.error("데이터 요청 중 오류 발생:", error);
+    }
+  };
 
   return (
     <>
